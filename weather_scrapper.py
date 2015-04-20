9012be29b1b4014ee8a8e6ac246a9b08ede43a58
mkleinert <matt.kleinert@gmail.com>
2015-04-20 16:01:10 -0400

    Test Commit

--- weather_scrapper.py
+++ weather_scrapper.py
@@ -1,1 +1,15 @@
 __author__ = 'mkleinert'
+
+from bs4 import BeautifulSoup
+
+import requests
+
+url = "api.wunderground.com/api/0f58c05cd95da63a/conditions/q/46231.json"
+
+r  = requests.get("http://" +url)
+
+data = r.text
+
+soup = BeautifulSoup(data)
+
+print(soup)
