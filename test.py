a1c6a402120070189ef31a1654146e9b86cc3437
mkleinert <matt.kleinert@gmail.com>
2015-04-20 10:51:12 -0400

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+__author__ = 'mkleinert'
+
+from bs4 import BeautifulSoup
+
+import requests
+
+url = input("Enter a website to extract the URL's from: ")
+
+r  = requests.get("http://" +url)
+
+data = r.text
+
+soup = BeautifulSoup(data)
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
